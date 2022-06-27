ea1553b4a207167a33fcd41f6c67c4ee29072afa
yeker <94519234+yeker12@users.noreply.github.com>
2022-06-27 14:54:30 +0900

    - completed Set up - created server.js (made a server)

---
+++ src/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+// PORT 정의
+const PORT = 4000;
+
+// 서버(app) 만들기: express 함수 호출로 가능
+const app = express();
+
+// 서버(app) GET request용 callback 함수 정의
+const handleHome = (req, res) => {
+   // 빈 화면으로 끝내기
+   // return res.end
+   // 화면에 글씨 보내기
+   return res.send("<h1>I still love you!</h1>"); 
+}
+
+const handleLogin = (req, res) => {
+   return res.send({"text": "Login Success!"});
+}
+
+// 서버 GET request 하기: route와 callback 함수 입력
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+
+// 서버(app) listening용 callback 함수 정의
+const handleListening = () => console.log(`Server is listening on http://localhost:${PORT}`);
+
+// 서버(app) listening 하기
+app.listen(PORT, handleListening);
