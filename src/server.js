13f5b88d166aa0f76d885d5c78ef033d9aba23e2
yeker <94519234+yeker12@users.noreply.github.com>
2022-06-28 20:05:54 +0900

    middleware 학습

--- src/server.js
+++ src/server.js
@@ -2,9 +2,14 @@ import express from "express";
 
 // PORT 정의
 const PORT = 4000;
-
 // 서버(app) 만들기: express 함수 호출로 가능
 const app = express();
+
+// middleware 정의
+const logger = (req, res, next) => {
+   console.log(`${req.method}: ${req.url}`);
+   next();
+}
 
 // 서버(app) GET request용 callback 함수 정의
 const handleHome = (req, res) => {
@@ -14,12 +19,31 @@ const handleHome = (req, res) => {
    return res.send("<h1>I still love you!</h1>"); 
 }
 
+// global middleware 정의
+const privateMiddleware = (req, res, next) => {
+   const url = req.url;
+   if(url === "/protected"){
+      res.send("<h1>Not allowed!❌</h1>");
+   }
+   next();
+}
+// object도 전송 가능
 const handleLogin = (req, res) => {
    return res.send({"text": "Login Success!"});
 }
+// privateMiddleware로 인해 실행 불가
+const handleProtected = (req, res) => {
+   return res.send("This is private Lounge!");
+}
+
+// app.use는 global middleware를 제공
+app.use(logger);
+app.use(privateMiddleware);
 
 // 서버 GET request 하기: route와 callback 함수 입력
 app.get("/", handleHome);
+// protected 접속했으나, 위에서 global middleware에서 막힘 (privateMiddleware)
+app.get("/protected", handleProtected);
 app.get("/login", handleLogin);
 
 
