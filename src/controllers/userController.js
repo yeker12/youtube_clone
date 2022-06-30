68d3d308be1b422ca57d9f19b8640dea22e3927e
yeker <94519234+yeker12@users.noreply.github.com>
2022-06-30 21:17:19 +0900

    controller 폴더 생성 및 정리

---
+++ src/controllers/userController.js
@@ -0,0 +1,3 @@
+export const joinUsers = (req, res) => res.send("Join");
+export const editUsers = (req, res) => res.send("Edit User!");
+export const removeUsers = (req, res) => res.send("Remove Users");
---
+++ src/controllers/videoController.js
@@ -0,0 +1,3 @@
+export const homeVideo = (req, res) => res.send("Home");
+export const watchVideos = (req, res) => res.send("Watch Videos!");
+export const editVideos = (req, res) => res.send("edit videos");
