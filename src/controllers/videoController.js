393d75644fcb5bb7a162e0a98ff47e14cd314c34
yeker <94519234+yeker12@users.noreply.github.com>
2022-07-10 18:43:43 +0900

    Promise 학습 (Callback 방식 대체)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,64 +1,37 @@
-let videos = [{
-    title: "FIrst video",
-    rating: 5,
-    comments: 3,
-    createdAt: "2 mins ago",
-    views:1,
-    id: 1, 
-},
-{
-    title: "Second video",
-    rating: 4.5,
-    comments: 5,
-    createdAt: "3 mins ago",
-    views:43,
-    id: 2,
-},
-{
-    title: "Third video",
-    rating: 3,
-    comments: 2,
-    createdAt: "4 mins ago",
-    views:88,
-    id: 3,
-}];
+import { trusted } from "mongoose";
+import VideoModel from "../models/Video";
 
-export const homeVideo = (req, res) => {
-    return res.render("home", {pageTitle: 'Home', videos});
+// 비디오 홈화면
+export const homeVideo = async(req, res) => {
+    try{
+        const video = await VideoModel.find({});
+        return res.render("home", {pageTitle: 'Home', videos: []});
+    } catch(error) {
+        return res.render("Server-Error: ", {error});
+    }
+    
 }
+// 비디오 보기 화면
 export const watchVideos = (req, res) => {
     const id = req.params.id; //url에서 가져온 id
-    const video = videos[id-1]; // videos 리스트에서 (id-1) 번째 객체
-    return res.render("watch", {pageTitle: `Watching ${video.title}`, video:video});
+    return res.render("watch", {pageTitle: `Watching` });
 }
-
-// 비디오 수정하기
+// 비디오 수정하기 (GET)
 export const getEdit = (req, res) => {
     const id = req.params.id; //url에서 가져온 id
-    const video = videos[id-1]; // videos 리스트에서 (id-1) 번째 객체  
-    return res.render("edit", {pageTitle: 'Edit', video:video });
+    return res.render("edit", {pageTitle: 'Edit'});
 }
+// 비디오 수정하기 (POST)
 export const postEdit = (req, res) => {
     const id = req.params.id;
-    const new_title = req.body.title;
-    videos[id-1].title = new_title;
     return res.redirect(`/videos/${id}`);
 }
-// 비디오 업로드하기
+// 비디오 업로드하기 (GET)
 export const getUpload = (req, res) => {
     return res.render("upload", {pageTitle: "Upload Video"});
 }
-
+// 비디오 업로드하기 (POST)
 export const postUpload = (req, res) => {
     const title = req.body.title;
-    const newVideo = {
-        title: title,
-        rating: 0,
-        comments: 0,
-        createdAt: "Just Now",
-        views:0,
-        id: videos.length + 1,
-    }
-    videos.push(newVideo);
-    return res.redirect("/");
+       return res.redirect("/");
 }
